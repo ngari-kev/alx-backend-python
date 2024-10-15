db3f2d201ba7060de30ee5248e2cb8cf820071f1
ngari-kev <kevngariwangui@gmail.com>
2024-10-15 19:53:12 +0300

    linting

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-#Use mypy to validate the following piece of code and apply any necessary changes.
+# Use mypy to validate the following piece of code and
+# apply any necessary changes.
 from typing import Tuple, List
+
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
