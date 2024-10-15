24dd9c10672638138bae235d53ecb80c41e5b92d
ngari-kev <kevngariwangui@gmail.com>
2024-10-15 19:25:30 +0300

    101-TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Achieves the instructions below"""
+# Given the parameters and the return values,
+# add type annotations to the function
+
+# Hint: look into TypeVar
+# def safely_get_value(dct, key, default = None):
+#     if key in dct:
+#         return dct[key]
+#     else:
+#         return default
+from typing import Any, Dict, TypeVar, Optional, Union
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Dict[Any, T],
+                     key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """returns either none or the value if Key is present"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
