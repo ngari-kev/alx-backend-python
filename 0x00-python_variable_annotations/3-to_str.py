8f4990d62c4da5d44ecd74c0fe9bd65b3778d7a2
ngari-kev <kevngariwangui@gmail.com>
2024-10-15 17:47:41 +0300

    annotated

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,6 @@ A module that contains a function returning string form of float number
 """
 
 
-def to_str(n: float) -> int:
+def to_str(n: float) -> str:
     """Accepts a float and returns a string"""
     return str(n)
